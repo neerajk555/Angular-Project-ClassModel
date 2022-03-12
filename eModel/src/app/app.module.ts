db1cdd8e13b24401fd36a5ea59f3cdd8830b6698
Muskan-Gandhi <muskangandhi73@gmail.com>
2022-03-12 16:43:37 +0530

    addd

--- eModel/src/app/app.module.ts
+++ eModel/src/app/app.module.ts
@@ -5,16 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InitialLoginComponent } from './initial-login/initial-login.component';
 import { ContainerWatchlistComponent } from './container-watchlist/container-watchlist.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InitialLoginComponent,
-    ContainerWatchlistComponent
+    ContainerWatchlistComponent,
+    UserRegistrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
