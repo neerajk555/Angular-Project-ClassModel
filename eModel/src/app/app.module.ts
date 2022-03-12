3565802d746a7ce194a570d1d8855249b5349830
nikunjkatariya <nikunjkatariya1999@gmail.com>
2022-03-12 16:24:14 +0530

    adding container watchlist component

--- eModel/src/app/app.module.ts
+++ eModel/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InitialLoginComponent } from './initial-login/initial-login.component';
+import { ContainerWatchlistComponent } from './container-watchlist/container-watchlist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InitialLoginComponent
+    InitialLoginComponent,
+    ContainerWatchlistComponent
   ],
   imports: [
     BrowserModule,
---
+++ eModel/src/app/container-watchlist/container-watchlist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-container-watchlist',
+  templateUrl: './container-watchlist.component.html',
+  styleUrls: ['./container-watchlist.component.css']
+})
+export class ContainerWatchlistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
