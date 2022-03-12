5f011cbb814e2c12109e0caf276cdd820bd10931
sarthakb408 <sarthakbhadiyadara408@gmail.com>
2022-03-12 16:18:54 +0530

    adding first component

--- eModel/src/app/app.module.ts
+++ eModel/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InitialLoginComponent } from './initial-login/initial-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InitialLoginComponent
   ],
   imports: [
     BrowserModule,
