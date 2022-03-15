a04103d70d6f6181462e98e18ce3c34361fc700f
Muskan-Gandhi <muskangandhi73@gmail.com>
2022-03-15 13:54:09 +0530

    loader added

--- eModel/src/app/app-routing.module.ts
+++ eModel/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
+import { ControlContainer } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminControlComponent } from './admin-control/admin-control.component';
+import { ContainerBookingComponent } from './container-booking/container-booking.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- eModel/src/app/app.component.ts
+++ eModel/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Event, Router, NavigationStart, NavigationEnd, RouterEvent } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'eModel';
+  showLoadingIndicator = true;
+  constructor(private _router: Router) {
+    this._router.events.subscribe((RouterEvent: Event) => {
+      if (RouterEvent instanceof NavigationStart) {
+        this.showLoadingIndicator = true;
+      }
+      if (RouterEvent instanceof NavigationEnd) {
+        this.showLoadingIndicator = false;
+      }
+    });
+  }
 }
