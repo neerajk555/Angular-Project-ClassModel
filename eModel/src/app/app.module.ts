1bf8ea50cf37890c9b6edaeeafaaf51ee2dee9b0
Naynesh335 <70320086+Naynesh335@users.noreply.github.com>
2022-03-17 23:09:09 +0530

    initial login page completed and signup page UI and validation completed

--- eModel/src/app/app.module.ts
+++ eModel/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,6 +32,9 @@ import { UpdatedateDirective } from './updatedate.directive';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
